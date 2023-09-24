5c44713bf1f1a5ee9b57a487c9c6b41cec894587
gacar <ozgurgokhanacar@gmail.com>
2023-09-24 14:09:26 +0300

    Initial commit

---
+++ discovery-server/src/test/java/com/acar/simplemicroservice/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acar.simplemicroservice.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ sms-service/src/test/java/com/acar/simplemicroservice/smsservice/SmsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acar.simplemicroservice.smsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
