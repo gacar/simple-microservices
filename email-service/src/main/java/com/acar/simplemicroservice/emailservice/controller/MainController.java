7bf537fdfe4b27c4ce249738c55eca9cefc13842
gacar <ozgurgokhanacar@gmail.com>
2023-09-30 23:57:26 +0300

    Apigateway is configured . . .

--- email-service/src/main/java/com/acar/simplemicroservice/emailservice/controller/MainController.java
+++ email-service/src/main/java/com/acar/simplemicroservice/emailservice/controller/MainController.java
@@ -1,14 +1,16 @@
 package com.acar.simplemicroservice.emailservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/email")
 public class MainController {
 
     @GetMapping("/sendEmail")
-    public String sendMessage(@RequestParam(value = "name", defaultValue = "Empty Message") String message) {
+    public String sendMessage(@RequestParam(value = "message", defaultValue = "Empty Message") String message) {
         System.out.println("email message is sending ...");
         return "Email is sending, Message:" + message;
     }
--- sms-service/src/main/java/com/acar/simplemicroservice/smsservice/controller/MainController.java
+++ sms-service/src/main/java/com/acar/simplemicroservice/smsservice/controller/MainController.java
@@ -1,14 +1,16 @@
 package com.acar.simplemicroservice.smsservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/sms")
 public class MainController {
 
     @GetMapping("/sendSms")
-    public String sendMessage(@RequestParam(value = "name", defaultValue = "Empty Message") String message) {
+    public String sendMessage(@RequestParam(value = "message", defaultValue = "Empty Message") String message) {
         System.out.println("email message is sending ...");
         return "SMS is sending, Message:" + message;
     }
